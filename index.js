8f6cc55f34f94a7a18ddd7807fe6089b709ed08b
Coral <coralpeony21@gmail.com>
2023-02-24 20:49:44 +0200

    added function questionsManager

--- index.js
+++ index.js
@@ -14,4 +14,39 @@ const team = [];
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-
+function questionsManager() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the team manager's name?",
+          name: "name",
+        },
+        {
+          type: "input",
+          message: "What is the team manager's ID?",
+          name: "id",
+        },
+        {
+          type: "input",
+          message: "What is the team manager's email?",
+          name: "email",
+        },
+        {
+          type: "input",
+          message: "What is the team manager's office number",
+          name: "officeNumber",
+        },
+      ])
+      .then((response) => {
+        const manager = new Manager(
+          response.name,
+          response.id,
+          response.email,
+          response.officeNumber
+        );
+        //console.log(manager);
+        team.push(manager);
+        addNextEmployee();
+      });
+  }
